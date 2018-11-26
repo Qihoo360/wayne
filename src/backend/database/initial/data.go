5f02f579321331adab2974958cf01902d3880422
chengyumeng <792400644@qq.com>
2018-11-26 10:45:46 +0800

    fix name: OAuth 2.0 Login

--- src/backend/database/initial/data.go
+++ src/backend/database/initial/data.go
@@ -4,7 +4,7 @@ package initial
 var InitialData = []string{
 	// -- config
 	`INSERT INTO  config  ( name,  value ) VALUES ('system.api-name-generate-rule', 'join');`,
-	`INSERT INTO  config  ( name,  value ) VALUES ('system.oauth2-title', 'Wayne Oauth 2.0 Login');`,
+	`INSERT INTO  config  ( name,  value ) VALUES ('system.oauth2-title', 'OAuth 2.0 Login');`,
 	// -- user
 	`INSERT INTO  user  ( id, name, email, display, comment, type, deleted, admin, last_login, last_ip, create_time, update_time, password, salt ) VALUES (1,'admin','admin@gmail.com','管理员','',0,0,1,now(),'127.0.0.1',now(),now(),'e7cadd50397b88397045bf1b7f406b34dc8dc6b8f79d470c0a80cf7aad08690748bf5e6c2d0881bb8bb9c96045b08318fa2b','BZoWKqwaQ6');`,
 	// -- namespace
