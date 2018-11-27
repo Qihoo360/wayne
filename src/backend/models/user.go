034fec74e50900f544d98aa82ca4aaab46004716
Frank Kung <kfanjian@gmail.com>
2018-11-27 17:24:10 +0800

    Replace DefalutString with DefaultInt64
    Signed-off-by: Frank Kung <kfanjian@gmail.com>

--- src/backend/models/user.go
+++ src/backend/models/user.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"strconv"
 	"time"
 
 	"github.com/Qihoo360/wayne/src/backend/util/encode"
@@ -94,11 +93,8 @@ func (*userModel) AddUser(m *User) (id int64, err error) {
 
 func addDefaultNamespace(user *User) (err error) {
 	// 添加默认命名空间开发者权限
-	demoNSId, err := strconv.ParseInt(beego.AppConfig.DefaultString("DemoNamespaceId", "0"), 10, 64)
-	if err != nil {
-		return
-	}
-	demoGroupId, err := strconv.ParseInt(beego.AppConfig.DefaultString("DemoGroupId", "0"), 10, 64)
+	demoNSId := beego.AppConfig.DefaultInt64("DemoNamespaceId", 1)
+	demoGroupId := beego.AppConfig.DefaultInt64("DemoGroupId", 1)
 	if err != nil {
 		return
 	}
