748f539abc0e9b3ebdf664399f6be1903abf3742
BennieMeng <BennieMeng@126.com>
2018-12-26 14:20:31 +0800

    frontend: resource limit i18n

--- src/frontend/src/app/shared/component/resource-limit/resource-limit.module.ts
+++ src/frontend/src/app/shared/component/resource-limit/resource-limit.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ResourceLimitComponent } from './resource-limit.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
-    FormsModule
+    FormsModule,
+    TranslateModule
   ],
   declarations: [
     ResourceLimitComponent
