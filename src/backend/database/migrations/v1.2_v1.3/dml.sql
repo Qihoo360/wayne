3818c09ea6ca8fffdbf16f9a871cc0592f525748
chengyumeng <792400644@qq.com>
2019-01-10 15:02:23 +0800

    backend:add update SQL

---
+++ src/backend/database/migrations/v1.2_v1.3/dml.sql
@@ -0,0 +1,12 @@
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('10', '127');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('10', '128');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('10', '129');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('10', '130');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('10', '131');
+
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('11', '128');
+
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('11', '129');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('11', '131');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('12', '129');
+INSERT INTO  group_permissions  ( group_id,  permission_id ) VALUES ('12', '131');
