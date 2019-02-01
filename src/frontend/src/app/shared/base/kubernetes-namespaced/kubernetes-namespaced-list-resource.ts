50e36fa1895b676a415283d9a7793cd57570f00c
wilhelmguo <shaowei3608@gmail.com>
2019-02-01 18:11:28 +0800

    frontend: add kubernetes namespaced resource base class

---
+++ src/frontend/src/app/shared/base/kubernetes-namespaced/kubernetes-namespaced-list-resource.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, Input, Output } from '@angular/core';
+import { ClrDatagridStateInterface } from '@clr/angular';
+import { Page } from '../../page/page-state';
+import { TplDetailService } from '../../tpl-detail/tpl-detail.service';
+
+export class KubernetesNamespacedListResource {
+  state: ClrDatagridStateInterface;
+  currentPage = 1;
+
+  @Input() page: Page;
+
+  @Output() paginate = new EventEmitter<ClrDatagridStateInterface>();
+  @Output() edit = new EventEmitter<any>();
+  @Output() delete = new EventEmitter<any>();
+
+  constructor(public tplDetailService: TplDetailService) {
+  }
+
+  versionDetail(version: string) {
+    this.tplDetailService.openModal(version, '详情');
+  }
+
+  onEditEvent(obj: any) {
+    this.edit.emit(obj);
+  }
+
+  onDeleteEvent(obj: any) {
+    this.delete.emit(obj);
+  }
+
+  refresh(state: ClrDatagridStateInterface) {
+    this.state = state;
+    this.paginate.emit(state);
+  }
+
+  pageSizeChange(pageSize: number) {
+    this.state.page.to = pageSize - 1;
+    this.state.page.size = pageSize;
+    this.currentPage = 1;
+    this.paginate.emit(this.state);
+  }
+}
---
+++ src/frontend/src/app/shared/base/kubernetes-namespaced/kubernetes-namespaced-resource.ts
@@ -0,0 +1,200 @@
+import { MessageHandlerService } from '../../message-handler/message-handler.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ConfirmationDialogService } from '../../confirmation-dialog/confirmation-dialog.service';
+import { AceEditorComponent } from '../../ace-editor/ace-editor.component';
+import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClusterService } from '../../client/v1/cluster.service';
+import { PageState } from '../../page/page-state';
+import { ClrDatagridStateInterface } from '@clr/angular';
+import { ConfirmationButtons, ConfirmationState, KubeResourceNamespace, KubeResourcePod, KubeResourcesName } from '../../shared.const';
+import { OnDestroy, OnInit } from '@angular/core';
+import { KubernetesClient } from '../../client/v1/kubernetes/kubernetes';
+import { ConfirmationMessage } from '../../confirmation-dialog/confirmation-message';
+
+export class KubernetesNamespacedResource implements OnInit, OnDestroy {
+  aceEditorModal: AceEditorComponent;
+
+  showState: object;
+  pageState: PageState = new PageState();
+
+  cluster: string;
+  clusters: Array<any>;
+  resourceName: string;
+  resources: Array<any>;
+  showList: any[] = Array();
+
+  subscription: Subscription;
+  resourceType: string;
+  kubeResource: KubeResourcesName;
+
+
+  namespaces: string[];
+  namespace: string;
+
+  constructor(public kubernetesClient: KubernetesClient,
+              public route: ActivatedRoute,
+              public router: Router,
+              public clusterService: ClusterService,
+              public authService: AuthService,
+              public messageHandlerService: MessageHandlerService,
+              public deletionDialogService: ConfirmationDialogService) {
+    this.subscription = deletionDialogService.confirmationConfirm$.subscribe(message => {
+      if (message &&
+        message.state === ConfirmationState.CONFIRMED &&
+        message.source === this.kubeResource) {
+        const obj: any = message.data;
+        this.kubernetesClient
+          .delete(this.cluster, this.kubeResource, obj.metadata.name, obj.metadata.namespace)
+          .subscribe(
+            response => {
+              this.retrieveResource();
+              this.messageHandlerService.showSuccess('删除成功！');
+            },
+            error => {
+              this.messageHandlerService.handleError(error);
+            }
+          );
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.initShow();
+    const cluster = this.route.snapshot.params['cluster'];
+    this.clusterService.getNames().subscribe(
+      response => {
+        const data = response.data;
+        this.clusters = data.map(item => item.name);
+        if (data && data.length > 0 && !cluster) {
+          this.router.navigateByUrl(`admin/kubernetes/${this.resourceType}/${data[0].name}`);
+          return;
+        }
+        if (cluster) {
+          this.jumpToHref(cluster);
+        }
+      },
+      error => this.messageHandlerService.handleError(error)
+    );
+  }
+
+  registResourceType(resourceType: string) {
+    this.resourceType = resourceType;
+  }
+
+  registKubeResource(kubeResource: string) {
+    this.kubeResource = kubeResource;
+  }
+
+  registShowSate(state: Object) {
+    this.showState = state;
+  }
+
+  onConfirmEvent() {
+    Object.keys(this.showState).forEach(key => {
+      if (this.showList.indexOf(key) > -1) {
+        this.showState[key] = {hidden: false};
+      } else {
+        this.showState[key] = {hidden: true};
+      }
+    });
+  }
+
+  onCancelEvent() {
+    this.initShow();
+  }
+
+  onEditResourceEvent(obj: any) {
+    this.kubernetesClient.get(this.cluster, this.kubeResource, obj.metadata.name, obj.metadata.namespace)
+      .subscribe(
+        response => {
+          const data = response.data;
+          this.aceEditorModal.openModal(data, `编辑 [${obj.metadata.name}]`, true);
+        },
+        error => this.messageHandlerService.handleError(error)
+      );
+  }
+
+  onSaveResourceEvent(obj: any) {
+    this.kubernetesClient.get(this.cluster, this.kubeResource, obj.metadata.name, obj.metadata.namespace).subscribe(
+      resp => {
+        const respObj = resp.data;
+        respObj.spec = obj.spec;
+        respObj.metadata.labels = obj.metadata.labels;
+        respObj.metadata.annotations = obj.metadata.annotations;
+        this.kubernetesClient.update(respObj, this.cluster, this.kubeResource, obj.metadata.name, obj.metadata.namespace).subscribe(
+          resp2 => {
+            this.messageHandlerService.showSuccess(`[${obj.metadata.name}] Updated!`);
+            this.retrieveResource();
+          },
+          error => {
+            this.messageHandlerService.handleError(error);
+          }
+        );
+      },
+      error => {
+        this.messageHandlerService.handleError(error);
+      }
+    );
+
+  }
+
+  initShow() {
+    this.showList = [];
+    Object.keys(this.showState).forEach(key => {
+      if (!this.showState[key].hidden) {
+        this.showList.push(key);
+      }
+    });
+  }
+
+  retrieveResource(state?: ClrDatagridStateInterface): void {
+    if (state) {
+      this.pageState = PageState.fromState(state, {totalPage: this.pageState.page.totalPage, totalCount: this.pageState.page.totalCount});
+    }
+    if (this.cluster) {
+      this.kubernetesClient.listPage(this.pageState, this.cluster, KubeResourcePod, this.namespace)
+        .subscribe(
+          response => {
+            const data = response.data;
+            this.resources = data.list;
+            this.pageState.page.totalPage = data.totalPage;
+            this.pageState.page.totalCount = data.totalCount;
+          },
+          error => this.messageHandlerService.handleError(error)
+        );
+    }
+  }
+
+  onDeleteResourceEvent(resource: any) {
+    const deletionMessage = new ConfirmationMessage(
+      '删除确认',
+      `是否确认删除对象 ${resource.metadata.name}`,
+      resource,
+      this.kubeResource,
+      ConfirmationButtons.DELETE_CANCEL
+    );
+    this.deletionDialogService.openComfirmDialog(deletionMessage);
+  }
+
+  jumpToHref(cluster) {
+    this.cluster = cluster;
+    this.router.navigateByUrl(`admin/kubernetes/${this.resourceType}/${cluster}`);
+    this.kubernetesClient.getNames(cluster, KubeResourceNamespace).subscribe(
+      resp => {
+        this.namespaces = Array<string>();
+        const namespaces: Array<any> = resp.data;
+        namespaces.map(ns => {
+          this.namespaces.push(ns.name);
+        });
+        this.namespace = this.namespaces && this.namespaces.length > 0 ? this.namespaces[0] : 'default';
+        // this.retrieveResource();
+      },
+      error => this.messageHandlerService.handleError(error)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
