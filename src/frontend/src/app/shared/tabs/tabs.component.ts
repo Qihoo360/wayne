3d815563f600e3fd0cf5e12e1e14003d399666b3
BennieMeng <BennieMeng@126.com>
2019-03-08 10:38:04 +0800

    frontend: change search section show logic

--- src/frontend/src/app/shared/tabs/tabs.component.ts
+++ src/frontend/src/app/shared/tabs/tabs.component.ts
@@ -53,10 +53,6 @@ export class TabsComponent implements AfterViewInit, OnDestroy {
     );
   }
 
-  get tabsNum(): number {
-    return this._tabs.length;
-  }
-
   get searchContent() {
     return this._searchContent;
   }
