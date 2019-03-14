8a643fcef2b3f5179a384a9d46ebaa1a1d43fd2d
BennieMeng <BennieMeng@126.com>
2019-03-14 11:17:11 +0800

    frontend: collapse component add & deployment-tpl deploy

--- src/frontend/src/app/shared/box/box.component.ts
+++ src/frontend/src/app/shared/box/box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'wayne-box',
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 })
 
 export class BoxComponent {
-
+  constructor(private el: ElementRef) {
+  }
+  @HostListener('mouseenter')
+  enterEvent() {
+    this.el.nativeElement.style.boxShadow = '0px 4px 8px 0px #ccc';
+  }
+  @HostListener('mouseleave')
+  leaveEvent() {
+    this.el.nativeElement.style.boxShadow = '0px 0px 1px 0px #ccc';
+  }
 }
