30d019d62930e3606ee177d8472fbf2b767f9598
jayryu <413485213@qq.com>
2019-10-23 16:09:02 +0800

    remove cobra

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,13 +1,10 @@
-package main
+ckage main
 
 import (
 	"github.com/Qihoo360/wayne/src/backend/cmd"
 )
 
-const Version = "1.6.1"
 
 func main() {
-	cmd.Version = Version
-
-	cmd.RootCmd.Execute()
+	cmd.Run()
 }
