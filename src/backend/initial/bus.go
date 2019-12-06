b236d543b0f1982ca6e78b803291b834113560fa
gsqGH <gengshiqiang@360.cn>
2019-12-06 16:21:01 +0800

    fix:delete rabbitMQ and change images registry

--- src/backend/initial/bus.go
+++ src/backend/initial/bus.go
@@ -1,14 +1,12 @@
 package initial
 
 import (
-	"github.com/Qihoo360/wayne/src/backend/bus"
 	"github.com/Qihoo360/wayne/src/backend/bus/newbus"
-	"github.com/astaxie/beego"
 )
 
 func InitBus() {
 	var err error
-	bus.DefaultBus, err = bus.NewBus(beego.AppConfig.String("BusRabbitMQURL"))
+	// bus.DefaultBus, err = bus.NewBus(beego.AppConfig.String("BusRabbitMQURL"))
 	if newbus.UBus == nil {
 		newbus.UBus, err = newbus.NewBus()
 		if err != nil {
