82ea097544e7dab2b425ce11b55ebb7b209bd876
BennieMeng <BennieMeng@126.com>
2020-01-14 18:25:20 +0800

    displayonly

--- src/frontend/src/app/shared/sidenav.const.ts
+++ src/frontend/src/app/shared/sidenav.const.ts
@@ -25,32 +25,11 @@ const adminSideNav: any[] = [
     type: SideNavType.GroupLink, icon: { shape: 'world', title: 'Kubernetes', solid: true }, text: 'Kubernetes', child: [
       { a: { link: 'kubernetes/node', text: 'Node', options: {exact: false}, icon: { shape: 'devices', solid: false } } },
       { a: { link: 'kubernetes/namespace', text: 'Namespace', options: {exact: false}, icon: { shape: 'vmw-app', solid: true } } },
-      { a: { link: 'kubernetes/customresourcedefinition', text: 'CRD', options: {exact: false}, icon: { shape: 'plugin', solid: true } } },
-      { type: SideNavType.Divider },
-      { a: { link: 'kubernetes/role', text: 'Role', options: {exact: false}, icon: { shape: 'assign-user' } } },
-      { a: { link: 'kubernetes/clusterrole', text: 'ClusterRole', options: {exact: false}, icon: { shape: 'assign-user', solid: true } } },
-      { a: { link: 'kubernetes/rolebinding', text: 'RoleBinding', options: {exact: false}, icon: { shape: 'administrator', solid: false } } },
-      { a: { link: 'kubernetes/clusterrolebinding', text: 'ClusterRoleBinding', options: {exact: false}, icon: { shape: 'administrator', solid: true } } },
-      { a: { link: 'kubernetes/serviceaccount', text: 'ServiceAccount', options: { exact: false }, icon: { shape: 'user', solid: false } } },
-      { type: SideNavType.Divider },
       { a: { link: 'kubernetes/deployment', text: 'Deployment', options: { exact: false }, icon: { shape: 'event', solid: false } } },
       { a: { link: 'kubernetes/replicaset', text: 'ReplicaSet', options: { exact: false }, icon: { shape: 'box-plot', solid: false } } },
-      { a: { link: 'kubernetes/statefulset', text: 'StatefulSet', options: { exact: false }, icon: { shape: 'blocks-group', solid: false } } },
-      { a: { link: 'kubernetes/daemonset', text: 'DaemonSet', options: { exact: false }, icon: { shape: 'layers', solid: false } } },
-      { a: { link: 'kubernetes/cronjob', text: 'CronJob', options: { exact: false }, icon: { shape: 'clock', solid: false } } },
-      { a: { link: 'kubernetes/job', text: 'Job', options: { exact: false }, icon: { shape: 'clock', solid: true } } },
       { a: { link: 'kubernetes/pod', text: 'Pod', options: { exact: false }, icon: { shape: 'tree', solid: true } } },
-      { a: { link: 'kubernetes/horizontalpodautoscaler', text: 'HPA.HPA', options: { exact: false }, icon: { shape: 'cloud-scale', solid: false } } },
-      { type: SideNavType.Divider },
       { a: { link: 'kubernetes/service', text: 'Service', options: { exact: false }, icon: { shape: 'network-globe', solid: false } } },
-      { a: { link: 'kubernetes/endpoint', text: 'Endpoint', options: { exact: false }, icon: { shape: 'tree-view', solid: false } } },
-      { a: { link: 'kubernetes/ingress', text: 'Ingress', options: { exact: false }, icon: { shape: 'network-switch', solid: false } } },
-      { type: SideNavType.Divider },
       { a: { link: 'kubernetes/configmap', text: 'ConfigMap', options: { exact: false }, icon: { shape: 'file-settings', solid: false } } },
-      { a: { link: 'kubernetes/secret', text: 'Secret', options: { exact: false }, icon: { shape: 'certificate', solid: false } } },
-      { a: { link: 'kubernetes/persistentvolume', text: 'PersistentVolume', options: { exact: false }, icon: { shape: 'storage', solid: false } } },
-      { a: { link: 'kubernetes/persistentvolumeclaim', text: 'MENU.PVC', options: { exact: false }, icon: { shape: 'data-cluster', solid: false } } },
-      { a: { link: 'kubernetes/storageclass', text: 'StorageClass', options: { exact: false }, icon: { shape: 'data-cluster', solid: true } } },
     ]
   },
   { type: SideNavType.Divider },
@@ -73,7 +52,6 @@ const adminSideNav: any[] = [
       { a: { link: 'system/user', text: 'MENU.USER_LIST', options: { exact: true } } },
       { a: { link: 'system/group', text: 'MENU.ROLE_LIST', options: { exact: true } } },
       { a: { link: 'system/permission', text: 'MENU.PERMISSION_LIST', options: { exact: true } } },
-      { a: { link: 'apikey', text: 'MENU.API_KEYS', options: { exact: true } } },
     ]
   },
   { type: SideNavType.Divider },
@@ -98,39 +76,6 @@ const adminSideNav: any[] = [
       { a: { link: 'deployment/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
     ]
   },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.STATEFULSET', shape: 'blocks-group' }, text: 'MENU.STATEFULSET', child: [
-      { a: { link: 'statefulset', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'statefulset/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'statefulset/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'statefulset/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.DAEMONSET', shape: 'layers' }, text: 'MENU.DAEMONSET', child: [
-      { a: { link: 'daemonset', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'daemonset/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'daemonset/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'daemonset/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.CRONJOB', shape: 'clock' }, text: 'MENU.CRONJOB', child: [
-      { a: { link: 'cronjob', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'cronjob/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'cronjob/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'cronjob/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
-  { type: SideNavType.Divider },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.AUTO_POD_SCALE', shape: 'cloud-scale' }, text: 'MENU.AUTO_POD_SCALE', child: [
-      { a: { link: 'hpa', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'hpa/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'hpa/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'hpa/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
   { type: SideNavType.Divider },
   {
     type: SideNavType.GroupLink, icon: { title: 'MENU.SERVICE', shape: 'network-globe' }, text: 'MENU.SERVICE', child: [
@@ -140,21 +85,6 @@ const adminSideNav: any[] = [
       { a: { link: 'service/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
     ]
   },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.EDGE_NODE_ABOUT', shape: 'cloud-traffic' }, text: 'MENU.EDGE_NODE_ABOUT', name: 'edge-node', child: [
-      { a: { link: 'service/edge-node', text: 'MENU.EDGE_NODE_CONFIG', options: { exact: true } } },
-      { a: { link: 'service/available-port', text: 'MENU.AVAILABLE_PORT_CONFIG', options: { exact: true } } },
-      { a: { link: 'service/used-port', text: 'MENU.USED_PORT', options: { exact: true } } },
-    ]
-  },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'Ingress', shape: 'network-switch' }, text: 'Ingress', child: [
-      { a: { link: 'ingress', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'ingress/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'ingress/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'ingress/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
   { type: SideNavType.Divider },
   {
     type: SideNavType.GroupLink, icon: { title: 'MENU.CONFIGMAP', shape: 'file-settings' }, text: 'MENU.CONFIGMAP', child: [
@@ -162,22 +92,6 @@ const adminSideNav: any[] = [
       { a: { link: 'configmap/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
       { a: { link: 'configmap/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
       { a: { link: 'configmap/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.SECRET', shape: 'certificate' }, text: 'MENU.SECRET', child: [
-      { a: { link: 'secret', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'secret/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'secret/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'secret/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
-    ]
-  },
-  {
-    type: SideNavType.GroupLink, icon: { title: 'MENU.PVC', shape: 'storage' }, text: 'MENU.PVC', child: [
-      { a: { link: 'persistentvolumeclaim', text: 'MENU.LIST', options: { exact: true } } },
-      { a: { link: 'persistentvolumeclaim/trash', text: 'MENU.RECYCLED', options: { exact: true } } },
-      { a: { link: 'persistentvolumeclaim/tpl', text: 'MENU.TEMPLATE_LIST', options: { exact: true } } },
-      { a: { link: 'persistentvolumeclaim/tpl/trash', text: 'MENU.TEMPLATE_RECYCLED', options: { exact: true } } },
     ]
   },
   { type: SideNavType.Divider },
